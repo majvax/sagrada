d689e9471c60f14a6a514af1c1fea57dac389708
majvax <majvax.contact@gmail.com>
2025-01-26 18:10:49 +0100

    added args to ensure compilation with C99

--- src/ui/color.h
+++ src/ui/color.h
@@ -7,18 +7,18 @@
 #define GREEN 3
 #define PURPLE 4
 
-#define ANSI_RESET "\e[0m"
-#define ANSI_RED "\e[0;31m"
-#define ANSI_GREEN "\e[0;32m"
-#define ANSI_YELLOW "\e[0;33m"
-#define ANSI_BLUE "\e[0;34m"
-#define ANSI_PURPLE "\e[0;35m"
+#define ANSI_RESET "\033[0m"
+#define ANSI_RED "\033[0;31m"
+#define ANSI_GREEN "\033[0;32m"
+#define ANSI_YELLOW "\033[0;33m"
+#define ANSI_BLUE "\033[0;34m"
+#define ANSI_PURPLE "\033[0;35m"
 
-#define BACKGROUND_RED "\e[41m"
-#define BACKGROUND_GREEN "\e[42m"
-#define BACKGROUND_YELLOW "\e[43m"
-#define BACKGROUND_BLUE "\e[44m"
-#define BACKGROUND_PURPLE "\e[45m"
+#define BACKGROUND_RED "\033[41m"
+#define BACKGROUND_GREEN "\033[42m"
+#define BACKGROUND_YELLOW "\033[43m"
+#define BACKGROUND_BLUE "\033[44m"
+#define BACKGROUND_PURPLE "\033[45m"
 
 extern char* get_ansi_code(int color);
 extern char* get_background_color_code(int color);
